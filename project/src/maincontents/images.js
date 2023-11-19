cb0d20d1806e15b0a88d9a1f74bb9f0ffb5efaa3
ys <dudtlsdl00@naver.com>
2023-11-19 22:33:12 +0900

    go

--- project/src/maincontents/images.js
+++ project/src/maincontents/images.js
@@ -1,35 +1,35 @@
 const images = [
   {
     source: "/main1.jpeg",
-    title: "Your Title Here" // Replace with your desired title
+    title: "징동 나와" // Replace with your desired title
   },
   {
     source: "/main2.jpeg",
-    title: "Your Title Here" // Replace with your desired title
+    title: "웨이보 나와" // Replace with your desired title
   },
   {
     source: "/main3.jpeg",
-    title: "Your Title Here" // Replace with your desired title
+    title: "뉴진스 나와" // Replace with your desired title
   },
   {
     source: "/main4.jpeg",
-    title: "Your Title Here" // Replace with your desired title
+    title: "lpl 비상" // Replace with your desired title
   },
   {
     source: "/main5.jpeg",
-    title: "Your Title Here" // Replace with your desired title
+    title: "고척돔 나와" // Replace with your desired title
   },
   {
     source: "/main6.jpeg",
-    title: "Your Title Here" // Replace with your desired title
+    title: "너 재능있어" // Replace with your desired title
   },
   {
     source: "/main7.png",
-    title: "Your Title Here" // Replace with your desired title
+    title: "그대로만해" // Replace with your desired title
   },
   {
     source: "/main8.jpeg",
-    title: "Your Title Here" // Replace with your desired title
+    title: "할 수 있어" // Replace with your desired title
   }
 ];
 
