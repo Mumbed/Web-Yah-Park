d856ac3ebf390a3eb4841215073b43fa73bb3798
ys <dudtlsdl00@naver.com>
2023-11-19 19:40:12 +0900

    slider upload

---
+++ project/src/maincontents/images.js
@@ -0,0 +1,36 @@
+const images = [
+  {
+    source: "/main1.jpeg",
+    title: "Your Title Here" // Replace with your desired title
+  },
+  {
+    source: "/main2.jpeg",
+    title: "Your Title Here" // Replace with your desired title
+  },
+  {
+    source: "/main3.jpeg",
+    title: "Your Title Here" // Replace with your desired title
+  },
+  {
+    source: "/main4.jpeg",
+    title: "Your Title Here" // Replace with your desired title
+  },
+  {
+    source: "/main5.jpeg",
+    title: "Your Title Here" // Replace with your desired title
+  },
+  {
+    source: "/main6.jpeg",
+    title: "Your Title Here" // Replace with your desired title
+  },
+  {
+    source: "/main7.png",
+    title: "Your Title Here" // Replace with your desired title
+  },
+  {
+    source: "/main8.jpeg",
+    title: "Your Title Here" // Replace with your desired title
+  }
+];
+
+export default images;
